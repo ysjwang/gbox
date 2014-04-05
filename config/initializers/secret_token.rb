0b82e24c0f0e9a38ad2d46f9533284ef90ed0b2d
Josiah W <ysjwang@Josiahs-MacBook-Pro.local>
2014-04-04 22:33:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gbox::Application.config.secret_key_base = '45136520684db2bf4843f35dd6f53cbb22c7cee467f67378946dd79c4b1c3f9e78252f4127c007b11dca5b42ea25d22d00a511662f3a572b30134f958c259dc0'
